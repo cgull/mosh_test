7c43787137479b3afaed7d0ed5a00abe1176bdf9
Kevin McKenzie <kmckenzi@stanford.edu>
2015-05-26 22:55:39 +0000

    added technology statistics based on real-world testing

--- run.sh
+++ run.sh
@@ -23,24 +23,14 @@ trace=term_trace_1
 
 iface=s0-eth1
 
-
-#for tech in 3G 4GLTE WIFI 3G_FLAKY 4GLTE_FLAKY WIFI_FLAKY; do
-#  for prog in MOSH SSH; do
-
-for tech in 3G; do
-for prog in MOSH; do
+for tech in 3G 3G_FLAKY 4G_LTE 4G_LTE_FLAKY WIFI; do
+for prog in SSH MOSH; do
   	dir=$rootdir/$tech
-
   	python mosh_test.py --dir $dir --tech $tech --prog $prog  --trace $trace --user $SUDO_USER --testdir $(pwd)/
     python plot_cdf.py $dir $tech
 
-	#python $plotpath/plot_queue.py -f $dir/qlen_$iface.txt -o $dir/q.png
-	#python $plotpath/plot_tcpprobe.py -f $dir/tcp_probe.txt -o $dir/cwnd.png --histogram
-  done
-  
+done  
 done
 
-#cat $rootdir/*/result.txt | sort -n -k 1
-#python plot-results.py --dir $rootdir -o $rootdir/result.png
 echo "Started at" $start
 echo "Ended at" `date`
