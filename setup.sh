1a48069a2cc908c4c41c51958c1f557f749cc600
Ubuntu <ubuntu@ip-172-31-26-188.us-west-2.compute.internal>
2015-05-29 15:11:45 +0000

    wrote up the instructions in the README

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install mosh
 sudo cpan IO::Pty::Easy
-ssh-keygen -t rsa -f ./private_test_key2 -q -N ""
+ssh-keygen -t rsa -f ./private_test_key -q -N ""
 cat private_test_key.pub >> ~/.ssh/authorized_keys 
