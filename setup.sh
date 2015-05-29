df19949eb3762678148c50693e993a39b95e795c
Kevin McKenzie <kjtmckenzie@gmail.com>
2015-05-29 05:37:31 +0000

    fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
+apt-get update
 apt-get install mosh
-yes "yes" | cpan IO::Pty::Easy
+yes "yes" | sudo cpan IO::Pty::Easy
 ssh-keygen -t rsa -f ./private_test_key2 -q -N ""
 cat private_test_key.pub >> ~/.ssh/authorized_keys 
